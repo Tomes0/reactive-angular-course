4ab3fe1be675e3ec368e265e1d833bdc8ce29195
kutlak.tamas <kutlakt@yahoo.com>
2022-10-26 09:33:09 +0200

    Separated out into presentational component

--- src/app/service/courses.service.ts
+++ src/app/service/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
 
 @Injectable({
@@ -14,8 +14,9 @@ export class CoursesService{
   loadAllCourses():Observable<Course[]> {
     return this.http.get<Course[]>('/api/courses')
       .pipe(
-        map(x => x['payload'])
-      )
+        map(x => x['payload']),
+        shareReplay()
+      );
 
   }
 }
